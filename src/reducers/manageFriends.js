70f43e6336e442c7750cdcd64c85f404a59b6305
Adriana <am20dipi@alum.siena.edu>
2021-07-23 17:01:04 -0400

    commit tree f1b9e70aa22571cf995e5798f60922633d392707

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,28 @@
 export function manageFriends(state, action){
+    state = {
+        friends: []
+    }
+
+    action = {
+        type: "ADD_FRIEND",
+        friend: {
+            name: "Chrome Boi",
+            hometown: "NYC",
+            id: 1
+        }
+    }
+
+    action = {
+        type: "REMOVE_FRIEND",
+        id: 1
+      }
+
+    switch(action.type){
+        case "ADD_FRIEND":
+            return {friends: state.friends + action.friend}
+        case "REMOVE_FRIEND":
+            return {friends: state.friends - action.friend}
+        default:
+            return state
+    }
 }
